b71628e51b4c5fcdff1ca3ccf8d76095ab6e811a
schaubal <schaubal@tx24seven.com>
2020-07-10 16:41:35 +0200

    done

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -1,35 +1,23 @@
+% predict the label of an input given a trained neural network
+
 function p = predict(Theta1, Theta2, X)
-%PREDICT Predict the label of an input given a trained neural network
-%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
-%   trained weights of a neural network (Theta1, Theta2)
+	m = size(X, 1);
+	num_labels = size(Theta2, 1);
 
-% Useful values
-m = size(X, 1);
-num_labels = size(Theta2, 1);
+	p = zeros(size(X, 1), 1);
 
-% You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+	% add an intercept term
+	X = [ones(m, 1) X];
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
+	% add intercept term to the output of the first layer
+	Layer1 = [ones(m, 1) sigmoid(X * Theta1')];
 
+	% add intercept term to the output of the first layer
+	probabilities = [sigmoid(Layer1 * Theta2')];
 
-
-
-
-
-
-
-
-% =========================================================================
-
+	for i = 1:m
+		[val idx] = max(probabilities(i, 1:end));
+		p(i) = idx;
+	end
 
 end
