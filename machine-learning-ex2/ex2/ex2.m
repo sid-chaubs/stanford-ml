27a2fbcc1b80b49df2ed1d10a35bb765ec2399cb
schaubal <schaubal@tx24seven.com>
2020-07-10 12:52:53 +0200

    finishing up

--- machine-learning-ex2/ex2/ex2.m
+++ machine-learning-ex2/ex2/ex2.m
@@ -15,14 +15,14 @@ initial_theta = zeros(n + 1, 1);
 options = optimset('GradObj', 'on', 'MaxIter', 400);
 [theta, cost] = fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
 
-fprintf('cost at theta found by fminunc: %f\n', cost);
-fprintf('theta: \n');
-fprintf('%f\n', theta);
+fprintf('Computed Cost: %f\n', cost);
+fprintf('\nComputed Weights: ');
+fprintf('%f ', theta);
 
 prob = sigmoid([1 45 85] * theta);
-fprintf('For a student with scores 45 and 85, we predict an admission probability of %f\n', prob);
+fprintf('\n\nFor a student with scores 45 and 85, we predict an admission probability of %f\n', prob);
 fprintf('Expected value: 0.775 +/- 0.002\n\n');
 
 p = predict(theta, X);
 fprintf('Training Accuracy: %f\n', mean(double(p == y)) * 100);
-fprintf('Expected accuracy (approx): 89.0\n');
+fprintf('Expected Accuracy (approx): 89.0\n\n');
--- machine-learning-ex2/ex2/ex2_reg.m
+++ machine-learning-ex2/ex2/ex2_reg.m
@@ -21,4 +21,4 @@ options = optimset('GradObj', 'on', 'MaxIter', 400);
 p = predict(theta, X);
 
 fprintf('Training accuracy: %f\n', mean(double(p == y)) * 100);
-fprintf('Expected accuracy (with lambda = 1): 83.1 (approx)\n');
+fprintf('Expected accuracy (with lambda = 1): 83.1 (approx)\n\n');
