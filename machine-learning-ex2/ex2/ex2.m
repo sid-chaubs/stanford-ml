ca77e290540ba05d00d85aff3ca489a3a4ca081f
schaubal <schaubal@tx24seven.com>
2020-07-10 12:48:49 +0200

    cleans up more code

--- machine-learning-ex2/ex2/ex2.m
+++ machine-learning-ex2/ex2/ex2.m
@@ -11,39 +11,18 @@ X = data(:, [1, 2]); y = data(:, 3);
 X = [ones(m, 1) X];
 initial_theta = zeros(n + 1, 1);
 
-% compute and display initial cost and gradient
-[cost, grad] = costFunction(initial_theta, X, y);
-
-fprintf('Cost at initial theta (zeros): %f\n', cost);
-fprintf('Expected cost (approx): 0.693\n');
-fprintf('Gradient at initial theta (zeros): \n');
-fprintf('%f \n', grad);
-fprintf('Expected gradients (approx):\n -0.1000\n -12.0092\n -11.2628\n\n');
-
-% compute and display cost and gradient with non-zero theta
-test_theta = [-24; 0.2; 0.2];
-[cost, grad] = costFunction(test_theta, X, y);
-
-fprintf('Cost at test theta: %f\n', cost);
-fprintf('Expected cost (approx): 0.218\n');
-fprintf('Gradient at test theta: \n');
-fprintf(' %f \n', grad);
-fprintf('Expected gradients (approx):\n 0.043\n 2.566\n 2.647\n\n');
-
 % compute cost with fminunc
 options = optimset('GradObj', 'on', 'MaxIter', 400);
 [theta, cost] = fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
 
-fprintf('Cost at theta found by fminunc: %f\n', cost);
-fprintf('Expected cost (approx): 0.203\n');
+fprintf('cost at theta found by fminunc: %f\n', cost);
 fprintf('theta: \n');
 fprintf('%f\n', theta);
-fprintf('Expected theta (approx):\n -25.161\n 0.206\n 0.201\n\n');
 
 prob = sigmoid([1 45 85] * theta);
 fprintf('For a student with scores 45 and 85, we predict an admission probability of %f\n', prob);
 fprintf('Expected value: 0.775 +/- 0.002\n\n');
 
 p = predict(theta, X);
-fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
+fprintf('Training Accuracy: %f\n', mean(double(p == y)) * 100);
 fprintf('Expected accuracy (approx): 89.0\n');
--- machine-learning-ex2/ex2/ex2_reg.m
+++ machine-learning-ex2/ex2/ex2_reg.m
@@ -4,33 +4,21 @@ clear;
 close all;
 clc;
 
+% load data
 data = load('ex2data2.txt');
 X = data(:, [1, 2]); y = data(:, 3);
 X = mapFeature(X(:,1), X(:,2));
 
-% set initial theta and lambda
+% set up initial weights and regularization parameter
 initial_theta = zeros(size(X, 2), 1);
 lambda = 1;
 
-[cost, grad] = costFunctionReg(initial_theta, X, y, lambda);
-
-fprintf('Cost at initial theta (zeros): %f\n', cost);
-fprintf('Gradient at initial theta (zeros) - first five values only:\n');
-fprintf(' %f \n', grad(1:5));
-
-% Compute and display cost and gradient with all-ones theta and lambda = 10
-test_theta = ones(size(X,2),1);
-[cost, grad] = costFunctionReg(test_theta, X, y, 10);
-
-fprintf('\nCost at test theta (with lambda = 10): %f\n', cost);
-
-initial_theta = zeros(size(X, 2), 1);
-lambda = 1;
+% configure options for fminunc
 options = optimset('GradObj', 'on', 'MaxIter', 400);
 [theta, J, exit_flag] = fminunc(@(t)(costFunctionReg(t, X, y, lambda)), initial_theta, options);
 
 % compute accuracy on our training set
 p = predict(theta, X);
 
-fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
+fprintf('Training accuracy: %f\n', mean(double(p == y)) * 100);
 fprintf('Expected accuracy (with lambda = 1): 83.1 (approx)\n');
